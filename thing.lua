029d383b629f93089af52576562f300c0376b318
insanity69642 <gergokarpat420@gmail.com>
2025-05-17 23:10:19 +0200

    thing.lua

---
+++ thing.lua
@@ -0,0 +1,5 @@
+local vluaasset = game.InsertService:LoadAsset(139755392718638)
+local vluascript = vluaasset:WaitForChild("vLua")
+vluascript.Parent = workspace
+vluaasset:Destroy()
+require(vluascript)([[print("hello world")]])()
